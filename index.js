452a18165b2bbd328900cd547c66b71236bd1c39
praphatsorndee-code <praphatsorn.dee@ku.th>
2026-01-27 11:49:16 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+const http = require('http');
+const host ='localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end('My First Server!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running at http://${host}:${port}`);
+});
